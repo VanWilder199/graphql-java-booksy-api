426cd7f3aaa7ce53acb758b717b27602a498d6f0
VanWilder199 <72692962+VanWilder199@users.noreply.github.com>
2023-10-15 12:13:10 +0200

    init

---
+++ src/main/java/com/copy/booksy/api/maingraphql/Application.java
@@ -0,0 +1,13 @@
+package com.copy.booksy.api.maingraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/copy/booksy/api/maingraphql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copy.booksy.api.maingraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
